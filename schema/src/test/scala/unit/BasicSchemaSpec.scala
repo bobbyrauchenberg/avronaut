6b20e10c20a59d4ba4d555ba64655e37bb825ed3
bobbyrauchenberg <bobbyrauchenberg@gmail.com>
2019-09-01 23:05:55 +0100

    Clean up basic schema test

--- schema/src/test/scala/unit/BasicSchemaSpec.scala
+++ schema/src/test/scala/unit/BasicSchemaSpec.scala
@@ -13,51 +13,53 @@ class BasicSchemaSpec extends ProperyChecksSpecBase {
   "schema" should {
 
     "be built for a basic null record" in new TestContext {
-      AvroSchema[SimpleNull].schema.map(_.toString) should beRight(simpleSchema("null"))
+      schemaAsString[SimpleNull] should beRight(simpleSchema("null"))
     }
 
     "be built for a basic boolean record" in new TestContext {
-      AvroSchema[SimpleBool].schema.map(_.toString) should beRight(simpleSchema("boolean"))
+      schemaAsString[SimpleBool] should beRight(simpleSchema("boolean"))
     }
 
     "be built for a basic string record" in new TestContext {
-      AvroSchema[SimpleString].schema.map(_.toString) should beRight(simpleSchema("string"))
+      schemaAsString[SimpleString] should beRight(simpleSchema("string"))
     }
 
     "be built for a basic int record" in new TestContext {
-      AvroSchema[SimpleInt].schema.map(_.toString) should beRight(simpleSchema("int"))
+      schemaAsString[SimpleInt] should beRight(simpleSchema("int"))
     }
 
     "be built for a basic long record" in new TestContext {
-      AvroSchema[SimpleLong].schema.map(_.toString) should beRight(simpleSchema("long"))
+      schemaAsString[SimpleLong] should beRight(simpleSchema("long"))
     }
 
     "be built for a basic float record" in new TestContext {
-      AvroSchema[SimpleFloat].schema.map(_.toString) should beRight(simpleSchema("float"))
+      schemaAsString[SimpleFloat] should beRight(simpleSchema("float"))
     }
 
     "be built for a basic double record" in new TestContext {
-      AvroSchema[SimpleDouble].schema.map(_.toString) should beRight(simpleSchema("double"))
+      schemaAsString[SimpleDouble] should beRight(simpleSchema("double"))
     }
 
     "be built for a basic bytes record" in new TestContext {
-      AvroSchema[SimpleByte].schema.map(_.toString) should beRight(simpleSchema("bytes"))
+      schemaAsString[SimpleByte] should beRight(simpleSchema("bytes"))
     }
   }
 
   trait TestContext {
+
+    def schemaAsString[T : AvroSchema] = AvroSchema[T].schema.map(_.toString)
+
     def simpleSchema(valueType: String) = s"""{"type":"record","fields":[{"name":"value","type":"$valueType"}]}"""
   }
 
 }
 
-sealed trait SimpleCase
-case class SimpleNull(value: Null) extends SimpleCase
-case class SimpleBool(value: Boolean) extends SimpleCase
-case class SimpleString(value: String) extends SimpleCase
-case class SimpleInt(value: Int) extends SimpleCase
-case class SimpleLong(value: Long) extends SimpleCase
-case class SimpleFloat(value: Float) extends SimpleCase
-case class SimpleDouble(value: Double) extends SimpleCase
-case class SimpleByte(value: Byte) extends SimpleCase
+case class SimpleNull(value: Null)
+case class SimpleBool(value: Boolean)
+case class SimpleString(value: String)
+case class SimpleInt(value: Int)
+case class SimpleLong(value: Long)
+case class SimpleFloat(value: Float)
+case class SimpleDouble(value: Double)
+case class SimpleByte(value: Byte)
 
