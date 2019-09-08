3869ef83346f9e8eec6f4b6f33a360f70f098178
bobbyrauchenberg <bobbyrauchenberg@gmail.com>
2019-09-08 20:38:09 +0100

    Sealed trait union types

--- decoder/src/main/scala/com/rauchenberg/cupcatAvro/decoder/Decoder.scala
+++ decoder/src/main/scala/com/rauchenberg/cupcatAvro/decoder/Decoder.scala
@@ -1,9 +1,14 @@
 package com.rauchenberg.cupcatAvro.decoder
 
-import magnolia.{CaseClass, Magnolia}
+import collection.JavaConverters._
+import com.rauchenberg.cupcatAvro.common._
+import magnolia.{CaseClass, Magnolia, SealedTrait, Subtype}
 import cats.implicits._
-import com.rauchenberg.cupcatAvro.common.Result
-import org.apache.avro.generic.GenericRecord
+import com.rauchenberg.cupcatAvro.common.{Error, Result}
+import org.apache.avro.Schema
+import org.apache.avro.generic.{GenericContainer, GenericData, GenericEnumSymbol, GenericRecord}
+
+import scala.reflect.runtime.universe
 
 trait Decoder[T] {
 
@@ -20,7 +25,6 @@ object Decoder {
   implicit def gen[T]: Typeclass[T] = macro Magnolia.gen[T]
 
   def combine[T](ctx: CaseClass[Typeclass, T]): Typeclass[T] = new Typeclass[T] {
-
     override def decodeFrom(fieldName: String, record: GenericRecord): Result[T] = {
       ctx.parameters.toList.traverse { param =>
         val decodeResult = param.typeclass.decodeFrom(param.label, record)
@@ -29,6 +33,51 @@ object Decoder {
           case (res, _) => res
         }
       }.map(ctx.rawConstruct(_))
+    }
+  }
+
+  import scala.reflect.runtime.universe._
+
+  def dispatch[T: TypeTag](ctx: SealedTrait[Typeclass, T]): Typeclass[T] = new Typeclass[T] {
+    override def decodeFrom(fieldName: String, record: GenericRecord): Result[T] = {
+
+      val recordValue = record.get(fieldName)
+
+      val schema = record.getSchema.getField(fieldName).schema()
+      schema.getType match {
+        case Schema.Type.UNION =>
+          recordValue match {
+            case container: GenericData.Record =>
+              val os = schema.getTypes.asScala.find(_.getFullName == container.getSchema.getFullName)
+              val subschema = os.headOption
+              ctx.subtypes.filter(_.typeName.full == container.getSchema.getFullName).headOption.flatMap { v: Subtype[Typeclass, T] =>
+                subschema.map { ss =>
+                  val gr = new GenericData.Record(ss)
+                  container.getSchema.getFields.asScala.toList.traverse { f =>
+                    safe(gr.put(f.name, container.get(f.name)))
+                  }.flatMap(_ => v.typeclass.decodeFrom(fieldName, gr))
+                    .leftMap(_ => Error("Was not able to create a sealed trait UNION, couldn't add fields to GenericData.Record"))
+                }
+              }.getOrElse(Error("Was not able to create a sealed trait UNION, couldn't find a subtype candidate").asLeft)
+             case _ =>
+               safe(
+                 ctx.subtypes.filter(_.typeName.full == recordValue.asInstanceOf[Schema].getFullName).map(toCaseObject)
+                 .headOption.map(_.asRight[Error]).getOrElse(Error(s"couldn't instantiate ENUM for field $fieldName").asLeft)
+               ).flatten
+          }
+        case Schema.Type.ENUM =>
+          ctx.subtypes.filter(_.typeName.short == recordValue).map(toCaseObject)
+            .headOption.map(_.asRight[Error]).getOrElse(Error(s"couldn't instantiate ENUM for field $fieldName").asLeft)
+        case other => Error(s"Unsupported sealed trait type : $other, only UNION and ENUM are supported").asLeft
+      }
+
+    }
+
+    def toCaseObject(v: Subtype[Typeclass, T]) = {
+      val runtimeMirror = universe.runtimeMirror(getClass.getClassLoader)
+      val module = runtimeMirror.staticModule(v.typeName.full)
+      val companion = runtimeMirror.reflectModule(module.asModule)
+      companion.instance.asInstanceOf[T]
     }
 
   }
--- decoder/src/test/scala/unit/decoder/CoproductSpec.scala
+++ decoder/src/test/scala/unit/decoder/CoproductSpec.scala
@@ -2,34 +2,40 @@ package unit.decoder
 
 
 import com.danielasfregola.randomdatagenerator.magnolia.RandomDataGenerator._
-import shapeless.ops.coproduct.Inject
-import shapeless.{:+:, CNil, Coproduct, Inl, Inr}
-import unit.decoder.utils.RunAssert.runAssert
-import utils.ShapelessArbitraries._
+import com.rauchenberg.cupcatAvro.decoder.{DecodeTo}
+import com.rauchenberg.cupcatAvro.schema.AvroSchema
+import com.rauchenberg.cupcatAvro.common._
+import org.apache.avro.generic.GenericData
+import shapeless.{:+:, CNil}
 import unit.common.UnitSpecBase
 
 class CoproductSpec extends UnitSpecBase {
 
   "decoder" should {
 
-    "decode a union expressed with a coproduct 1" in {
-      forAll { b: Boolean =>
-        runAssert(b, Union(Coproduct[Boolean :+: Int :+: String :+: CNil](b)))
+    "decode a union expressed with a coproduct" in new TestContext {
+      import unit.decoder.utils.ShapelessArbitraries._
+
+      forAll { (cp: Boolean :+: Int :+: String :+: CNil) =>
+        runCoproductAssert(Union(cp))
       }
     }
 
-    "decode a union expressed with a coproduct 2" in {
-      forAll { b: Int =>
-        runAssert(b, Union(Coproduct[Boolean :+: Int :+: String :+: CNil](b)))
+    trait TestContext {
+      def runCoproductAssert(expected: Union) = {
+
+        val schema = AvroSchema[Union].schema
+
+        val record = new GenericData.Record(schema.value)
+
+        val field =
+          expected.field.select[Boolean].orElse(expected.field.select[Int]).orElse(expected.field.select[String]).get
+
+        record.put("field", field)
+
+        DecodeTo[Union](record) should beRight(expected)
       }
     }
-
-    "decode a union expressed with a coproduct 3" in {
-      forAll { b: String =>
-        runAssert(b, Union(Coproduct[Boolean :+: Int :+: String :+: CNil](b)))
-      }
-    }
-
 
   }
 
