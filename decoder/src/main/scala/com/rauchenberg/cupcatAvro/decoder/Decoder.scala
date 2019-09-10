058b63e547146fb8f7a5ea2b51d037b1ecf147c8
bobbyrauchenberg <bobbyrauchenberg@gmail.com>
2019-09-10 11:45:07 +0100

    Cleanup decoder, add helper and add sbt script

--- decoder/src/main/scala/com/rauchenberg/cupcatAvro/decoder/Decoder.scala
+++ decoder/src/main/scala/com/rauchenberg/cupcatAvro/decoder/Decoder.scala
@@ -2,13 +2,15 @@ package com.rauchenberg.cupcatAvro.decoder
 
 import cats.implicits._
 import com.rauchenberg.cupcatAvro.common.annotations.SchemaAnnotations._
-import com.rauchenberg.cupcatAvro.common.{Error, Result, _}
+import com.rauchenberg.cupcatAvro.common.{Error, Result}
 import com.rauchenberg.cupcatAvro.decoder.helpers.ReflectionHelpers._
 import magnolia.{CaseClass, Magnolia, SealedTrait}
 import org.apache.avro.Schema
 import org.apache.avro.generic.{GenericData, GenericRecord}
+import com.rauchenberg.cupcatAvro.decoder.helpers.SealedTraitUnionDispatcher._
+import com.rauchenberg.cupcatAvro.decoder.helpers.{UnionEnum, UnionRecord}
 
-import scala.collection.JavaConverters._
+import scala.reflect.runtime.universe._
 
 trait Decoder[T] {
 
@@ -38,56 +40,24 @@ object Decoder {
     }
   }
 
-  import scala.reflect.runtime.universe._
 
   def dispatch[T: TypeTag](ctx: SealedTrait[Typeclass, T]): Typeclass[T] = new Typeclass[T] {
     override def decodeFrom(fieldName: String, record: GenericRecord): Result[T] = {
 
-      val unionErrorMsg = "Was not able to create a sealed trait UNION"
       val enumErrorMsg = "couldn't instantiate ENUM for field"
 
       val recordValue = record.get(fieldName)
       val schema = record.getSchema.getField(fieldName).schema()
 
-      def findSubtypeMatching(typeToMatch: String) = ctx.subtypes.filter(_.typeName.full == typeToMatch).headOption
-
       schema.getType match {
         case Schema.Type.UNION =>
           recordValue match {
-            case container: GenericData.Record =>
-
-              val recordSchema = container.getSchema
-              val schemaFullName = recordSchema.getFullName
-              val maybeSubschema = schema.getTypes.asScala.find(_.getFullName == schemaFullName).headOption
-
-              def createSubRecord(subSchema: Schema): Either[Error, GenericData.Record] = {
-                val gr = new GenericData.Record(subSchema)
-                recordSchema.getFields.asScala.toList
-                  .traverse(f => safe(gr.put(f.name, container.get(f.name)))).map(_ => gr)
-              }
-
-              (for {
-                subSchema <- maybeSubschema
-                subType <- findSubtypeMatching(schemaFullName)
-              } yield {
-                (for {
-                  subRecord <- createSubRecord(subSchema)
-                  decoded <- subType.typeclass.decodeFrom(fieldName, subRecord)
-                } yield decoded).leftMap(_ => Error(s"$unionErrorMsg, couldn't create the GenericData.Record subrecord"))
-              }).getOrElse(Error(s"$unionErrorMsg, couldn't find a subtype candidate").asLeft)
-
-            case _ =>
-              safe(
-                findSubtypeMatching(recordValue.asInstanceOf[Schema].getFullName)
-                  .map(st => toCaseObject[T](st.typeName.full))
-                  .map(_.asRight[Error])
-                  .getOrElse(Error(s"$enumErrorMsg $fieldName").asLeft)
-              ).flatten
+            case container: GenericData.Record => dispatchUnionRecord(UnionRecord(container, fieldName, schema, ctx))
+            case _ => dispatchUnionEnum(UnionEnum(fieldName, record, ctx))
           }
         case Schema.Type.ENUM =>
           ctx.subtypes.filter(_.typeName.short == recordValue)
-            .map(st => toCaseObject[T](st.typeName.full))
-            .headOption.map(_.asRight[Error]).getOrElse(Error(s"$enumErrorMsg $fieldName").asLeft)
+            .map(st => toCaseObject[T](st.typeName.full)).headOption.toRight(Error(s"$enumErrorMsg $fieldName"))
         case other => Error(s"Unsupported sealed trait type : $other, only UNION and ENUM are supported").asLeft
       }
 
---
+++ decoder/src/main/scala/com/rauchenberg/cupcatAvro/decoder/helpers/SealedTraitUnionDispatcher.scala
@@ -0,0 +1,62 @@
+package com.rauchenberg.cupcatAvro.decoder.helpers
+
+import cats.implicits._
+import com.rauchenberg.cupcatAvro.common.{Error, safe}
+import com.rauchenberg.cupcatAvro.decoder.Decoder.Typeclass
+import com.rauchenberg.cupcatAvro.decoder.helpers.ReflectionHelpers.toCaseObject
+import magnolia.SealedTrait
+import org.apache.avro.Schema
+import org.apache.avro.generic.{GenericData, GenericRecord}
+
+import scala.collection.JavaConverters._
+
+sealed trait SealedTraitUnion
+
+case class UnionRecord[T](genericRecord: GenericData.Record, fieldName: String, schema: Schema, ctx: SealedTrait[Typeclass, T]) extends SealedTraitUnion
+
+case class UnionEnum[T](fieldName: String, record: GenericRecord, ctx: SealedTrait[Typeclass, T]) extends SealedTraitUnion
+
+
+object SealedTraitUnionDispatcher {
+
+  val unionErrorMsg = "Was not able to create a sealed trait UNION"
+  val enumErrorMsg = "couldn't instantiate ENUM for field"
+
+  def dispatchUnionRecord[T](union: UnionRecord[T]): Either[Error, T] = {
+    val recordSchema = union.genericRecord.getSchema
+    val schemaFullName = recordSchema.getFullName
+
+    val matchFullName : Schema => Boolean = _.getFullName == schemaFullName
+
+    val findSubSchema = union.schema.getTypes.asScala.find(matchFullName).headOption.toRight(
+      Error(s"$unionErrorMsg, couldn't find a matching subschema")
+    )
+
+    def createRecordToDecode(subSchema: Schema): Either[Error, GenericData.Record] = {
+      val gr = new GenericData.Record(subSchema)
+      recordSchema.getFields.asScala.toList
+        .traverse(f => safe(gr.put(f.name, union.genericRecord.get(f.name)))).map(_ => gr)
+    }
+
+    for {
+      subSchema <- findSubSchema
+      subType <- findSubtypeToDispatch(schemaFullName, union.ctx)
+      subRecord <- createRecordToDecode(subSchema)
+      decodedT <- subType.typeclass.decodeFrom(union.fieldName, subRecord).map(_.asInstanceOf[T])
+    } yield decodedT
+  }
+
+  def dispatchUnionEnum[T](union: UnionEnum[T]) =
+    safe(
+      findSubtypeToDispatch(union.record.get(union.fieldName).asInstanceOf[Schema].getFullName, union.ctx)
+        .map(st => toCaseObject[T](st.typeName.full))
+        .leftMap(_ => Error(s"$enumErrorMsg ${union.fieldName}"))
+    ).flatten
+
+  private def findSubtypeToDispatch[T](typeToMatch: String, ctx: SealedTrait[Typeclass, T]) =
+    ctx.subtypes.filter(_.typeName.full == typeToMatch).headOption.toRight(
+      Error(s"$unionErrorMsg, couldn't find subtype matching $typeToMatch")
+    )
+
+
+}
