0dafee571e24a38ce151c57c12298e479068e18b
Robert Glyde <robert.glyde@sky.uk>
2019-09-11 15:59:45 +0100

    tested error returning as left for bad schema encoding

--- encoder/src/main/scala/com/rauchenberg/cupcatAvro/encoder/Encoder.scala
+++ encoder/src/main/scala/com/rauchenberg/cupcatAvro/encoder/Encoder.scala
@@ -1,14 +1,16 @@
 package com.rauchenberg.cupcatAvro.encoder
 
 import cats.implicits._
-import com.rauchenberg.cupcatAvro.common.Result
-import com.rauchenberg.cupcatAvro.schema.AvroSchema
+import com.rauchenberg.cupcatAvro.common.{Error, Result}
 import magnolia.{CaseClass, Magnolia}
+import org.apache.avro.Schema
 import org.apache.avro.generic.{GenericData, GenericRecord}
+
+import scala.collection.JavaConverters._
 
 trait Encoder[T] {
 
-  def encode(value: T): Result[Any]
+  def encode(value: T, schema: Schema): Result[Any]
 
 }
 
@@ -20,16 +22,24 @@ object Encoder {
 
   implicit def gen[T]: Typeclass[T] = macro Magnolia.gen[T]
 
-  def combine[T](ctx: CaseClass[Typeclass, T])(implicit avroSchema: AvroSchema[T]): Typeclass[T] = new Typeclass[T] {
-    override def encode(value: T): Result[GenericRecord] = {
-      ctx.parameters.map(p => p.typeclass.encode(p.dereference(value))).toList.sequence
-        .flatMap { results =>
-          avroSchema.schema.map { schema =>
+  def combine[T](ctx: CaseClass[Typeclass, T]): Typeclass[T] = new Typeclass[T] {
+    override def encode(value: T, schema: Schema): Result[GenericRecord] = {
+      val encoded =
+        for {
+          field <- schema.getFields.asScala
+          param <- ctx.parameters.find(_.label == field.name)
+        } yield param.typeclass.encode(param.dereference(value), field.schema)
+
+      encoded.toList match {
+        case Nil =>
+          Error(s"Invalid schema: $schema, for value: $value").asLeft
+        case enc =>
+          enc.sequence.map { results =>
             val record = new GenericData.Record(schema)
             results.zipWithIndex.foreach { case (r, i) => record.put(i, r) }
             record
           }
-        }
+      }
     }
   }
 
