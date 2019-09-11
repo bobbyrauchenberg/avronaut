f9be419c7184d5f21e444f1edfd285b73f99c69f
Robert Glyde <robert.glyde@sky.uk>
2019-09-11 21:23:44 +0100

    matching instance and implicit placement/extension

--- encoder/src/test/scala/unit/common/UnitSpecBase.scala
+++ encoder/src/test/scala/unit/common/UnitSpecBase.scala
@@ -1,10 +1,9 @@
 package unit.common
 
 import cats.scalatest.{EitherMatchers, EitherValues}
-import com.rauchenberg.cupcatAvro.encoder.instances.encoderInstances
 import com.rauchenberg.cupcatAvro.schema.instances.schemaInstances
 import org.scalatest.{Matchers, WordSpecLike}
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 trait UnitSpecBase extends WordSpecLike with ScalaCheckPropertyChecks with Matchers
-  with EitherMatchers with EitherValues with encoderInstances with schemaInstances
+  with EitherMatchers with EitherValues with schemaInstances
