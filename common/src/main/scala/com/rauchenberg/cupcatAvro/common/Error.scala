579bf6d205c2e960a6d33ae9a7e6d686e201f358
Robert Glyde <robert.glyde@sky.uk>
2019-09-11 20:33:51 +0100

    Added error type for aggregation and collecting errors during encoding

--- common/src/main/scala/com/rauchenberg/cupcatAvro/common/Error.scala
+++ common/src/main/scala/com/rauchenberg/cupcatAvro/common/Error.scala
@@ -1,3 +1,14 @@
 package com.rauchenberg.cupcatAvro.common
 
-case class Error(msg: String)
+import org.apache.avro.Schema
+
+sealed trait AvroError extends Product with Serializable
+
+object AvroError {
+
+  def encoderErrorMsg(schema: Schema, value: String) = s"Invalid schema: $schema, for value: $value"
+
+}
+
+case class Error(msg: String) extends AvroError
+case class AggregatedError(errors: List[AvroError]) extends AvroError
