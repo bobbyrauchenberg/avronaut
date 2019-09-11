0d9c92161008e1f2606ba8a4a66f0e750a56d92a
Robert Glyde <robert.glyde@sky.uk>
2019-09-11 09:58:45 +0100

    adding test for nested case classes and some refactoring to encoder signature

--- encoder/src/main/scala/com/rauchenberg/cupcatAvro/encoder/Encoder.scala
+++ encoder/src/main/scala/com/rauchenberg/cupcatAvro/encoder/Encoder.scala
@@ -2,15 +2,13 @@ package com.rauchenberg.cupcatAvro.encoder
 
 import cats.implicits._
 import com.rauchenberg.cupcatAvro.common.Result
+import com.rauchenberg.cupcatAvro.schema.AvroSchema
 import magnolia.{CaseClass, Magnolia}
-import org.apache.avro.Schema
 import org.apache.avro.generic.{GenericData, GenericRecord}
-
-import scala.collection.JavaConverters._
 
 trait Encoder[T] {
 
-  def encode(value: T, schema: Schema): Result[Any]
+  def encode(value: T): Result[Any]
 
 }
 
@@ -22,19 +20,15 @@ object Encoder {
 
   implicit def gen[T]: Typeclass[T] = macro Magnolia.gen[T]
 
-  def combine[T](ctx: CaseClass[Typeclass, T]): Typeclass[T] = new Typeclass[T] {
-    override def encode(value: T, schema: Schema): Result[GenericRecord] = {
-      val encoded =
-        for {
-        field <- schema.getFields.asScala
-        param <- ctx.parameters.find(_.label == field.name)
-      } yield param.typeclass.encode(param.dereference(value), field.schema)
-
-      encoded.toList.sequence
-        .map { results =>
-          val record = new GenericData.Record(schema)
-          results.zipWithIndex.foreach { case (r, i) => record.put(i, r) }
-          record
+  def combine[T](ctx: CaseClass[Typeclass, T])(implicit avroSchema: AvroSchema[T]): Typeclass[T] = new Typeclass[T] {
+    override def encode(value: T): Result[GenericRecord] = {
+      ctx.parameters.map(p => p.typeclass.encode(p.dereference(value))).toList.sequence
+        .flatMap { results =>
+          avroSchema.schema.map { schema =>
+            val record = new GenericData.Record(schema)
+            results.zipWithIndex.foreach { case (r, i) => record.put(i, r) }
+            record
+          }
         }
     }
   }
