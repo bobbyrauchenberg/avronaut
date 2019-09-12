cc9a700d99177d4dc124760429266a7594ab225e
bobbyrauchenberg <bobbyrauchenberg@gmail.com>
2019-09-12 14:51:36 +0100

    formatting

--- encoder/src/main/scala/com/rauchenberg/cupcatAvro/encoder/Encoder.scala
+++ encoder/src/main/scala/com/rauchenberg/cupcatAvro/encoder/Encoder.scala
@@ -9,10 +9,9 @@ import org.apache.avro.generic.GenericData
 
 import scala.collection.JavaConverters._
 
-
 sealed trait Encoded
-case class Primitive[T](primitive: T) extends Encoded
-case class Record(genericRecord: GenericData.Record) extends Encoded
+case class Primitive[T](primitive: T)                        extends Encoded
+case class Record(genericRecord: GenericData.Record)         extends Encoded
 case class Elements(elements: List[Encoded], schema: Schema) extends Encoded
 
 trait Encoder[T] {
@@ -29,15 +28,16 @@ object Encoder {
 
   implicit def gen[T]: Typeclass[T] = macro Magnolia.gen[T]
 
-  def combine[T](ctx: CaseClass[Typeclass, T]): Typeclass[T] = new Typeclass[T] {
-    override def encode(value: T, schema: Schema): Result[Encoded] = {
-      ((for {
-        field <- schema.getFields.asScala
-        param <- ctx.parameters.find(_.label == field.name)
-      } yield param.typeclass.encode(param.dereference(value), field.schema)))
-        .toList.sequence.map(l => Elements(l, schema))
-    }.leftMap(_ => encoderErrorFor(schema, value.toString))
-  }
+  def combine[T](ctx: CaseClass[Typeclass, T]): Typeclass[T] =
+    new Typeclass[T] {
+      override def encode(value: T, schema: Schema): Result[Encoded] = {
+        ((for {
+            field <- schema.getFields.asScala
+            param <- ctx.parameters.find(_.label == field.name)
+          } yield param.typeclass.encode(param.dereference(value), field.schema))
+          ).toList.sequence.map(l => Elements(l, schema))
+      }.leftMap(_ => encoderErrorFor(schema, value.toString))
+    }
 
 }
 
@@ -46,10 +46,12 @@ object EncoderPimps {
 
     def recur(e: Encoded, acc: List[Any]): List[Any] = e match {
       case Primitive(p) => acc :+ p
-      case Record(g) => acc :+ g
+      case Record(g)    => acc :+ g
       case Elements(e, schema) =>
         val record = new GenericData.Record(schema)
-        e.flatMap(v => recur(v, acc)).zipWithIndex.foreach { case (r, i) => record.put(i, r) }
+        e.flatMap(v => recur(v, acc)).zipWithIndex.foreach {
+          case (r, i) => record.put(i, r)
+        }
         acc :+ record
     }
 
@@ -61,7 +63,8 @@ object EncoderPimps {
         r.map(_ => record)
       }
       case Left(error) => error.asLeft
-      case _ => Error("you can only convert Elements to a GenericData.Record").asLeft
+      case _ =>
+        Error("you can only convert Elements to a GenericData.Record").asLeft
     }
   }
 
--- encoder/src/test/scala/unit/encoder/SimpleRecordSpec.scala
+++ encoder/src/test/scala/unit/encoder/SimpleRecordSpec.scala
@@ -14,7 +14,7 @@ class SimpleRecordSpec extends UnitSpecBase {
   "encoder" should {
     "encode a case class with supported primitives to a suitable record" in {
       forAll { data: TestRecord =>
-        val schema = AvroSchema[TestRecord].schema.value
+        val schema         = AvroSchema[TestRecord].schema.value
         val expectedRecord = new GenericData.Record(schema)
         expectedRecord.put("string", data.string)
         expectedRecord.put("boolean", data.boolean)
@@ -24,31 +24,25 @@ class SimpleRecordSpec extends UnitSpecBase {
         expectedRecord.put("double", data.double)
         expectedRecord.put("bytes", data.bytes)
 
-        Encoder[TestRecord].encode(data, schema).toGenRecord shouldBe expectedRecord.asRight
+        Encoder[TestRecord]
+          .encode(data, schema)
+          .toGenRecord shouldBe expectedRecord.asRight
       }
     }
 
     "encode a case class with nested primitives to a suitable record" in {
       forAll { data: NestedRecord =>
-        val schema = AvroSchema[NestedRecord].schema.value
+        val schema         = AvroSchema[NestedRecord].schema.value
         val expectedRecord = new GenericData.Record(schema)
-        val innerRecord = new GenericData.Record(AvroSchema[Inner].schema.value)
+        val innerRecord    = new GenericData.Record(AvroSchema[Inner].schema.value)
         innerRecord.put("value", data.inner.value)
         expectedRecord.put("string", data.string)
         expectedRecord.put("boolean", data.boolean)
         expectedRecord.put("inner", innerRecord)
 
-        Encoder[NestedRecord].encode(data, schema).toGenRecord shouldBe expectedRecord.asRight
-      }
-    }
-
-    "return an error when an invalid schema is given" in {
-      forAll { data: TestRecord =>
-        case class Broken(somethingElse: String)
-        val badSchema = AvroSchema[Broken].schema.value
-
-        val expectedErrorMsg = s"Invalid schema: $badSchema, for value: $data"
-        Encoder[TestRecord].encode(data, badSchema).toGenRecord shouldBe Error(expectedErrorMsg).asLeft
+        Encoder[NestedRecord]
+          .encode(data, schema)
+          .toGenRecord shouldBe expectedRecord.asRight
       }
     }
 
@@ -59,12 +53,20 @@ class SimpleRecordSpec extends UnitSpecBase {
         val badSchema = AvroSchema[Broken].schema.value
 
         val expectedErrorMsg = s"Invalid schema: $badSchema, for value: $data"
-        Encoder[NestedRecord].encode(data, badSchema).toGenRecord shouldBe Error(expectedErrorMsg).asLeft
+        Encoder[NestedRecord]
+          .encode(data, badSchema)
+          .toGenRecord shouldBe Error(expectedErrorMsg).asLeft
       }
     }
   }
 
-  case class TestRecord(string: String, boolean: Boolean, int: Int, long: Long, float: Float, double: Double, bytes: Array[Byte])
+  case class TestRecord(string: String,
+                        boolean: Boolean,
+                        int: Int,
+                        long: Long,
+                        float: Float,
+                        double: Double,
+                        bytes: Array[Byte])
   case class NestedRecord(string: String, boolean: Boolean, inner: Inner)
   case class Inner(value: String)
 }
